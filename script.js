438a4189941ad96958309dcc66cb29d0ddc7c7eb
singhvaibhav16 <singhvaibhav1606@gmail.com>
2025-02-01 12:20:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("heheh");
+//types of variables
+//primitive datatypes
+a="string" //string
+b=123123 //int or number
+bol=false // true or fales
+n=null //
+//non-primitive
+arr=[1,"fhue",567];
+let o={
+        vaibhav :{
+        course:"cse core",
+        roll:"245ucs229"
+        }
+};
